86b457db5d9c430f0050e5f5b45b12e1103f9559
iAcay <96024046+iAcay@users.noreply.github.com>
2023-11-02 21:32:53 +0100

    lead_tracker_chrome_extension

--- js/lead_tracker_chrome_extension/index.js
+++ js/lead_tracker_chrome_extension/index.js
@@ -1,17 +1,34 @@
 let leads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const leadsEl = document.getElementById("leads-el")
 
-localStorage.setItem("myLeads", "")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("leads"))
+
+if(leadsFromLocalStorage) {
+  leads = leadsFromLocalStorage
+  renderLeads(leads)
+}
 
 inputBtn.addEventListener("click", function() {
-  leads.push(inputEl.value)
-  inputEl.value = ""
-  renderLeads()
+  if(inputEl.value.length > 0) {
+    leads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem('leads', JSON.stringify(leads))
+  }
+  renderLeads(leads)
 })
 
-function renderLeads() {
+deleteBtn.addEventListener("click", function() {
+  if(confirm("Delete all leads?")) {
+    localStorage.clear();
+    leads = []
+  }
+  renderLeads(leads);
+})
+
+function renderLeads(leads) {
   let listItems = ""
   for (let i = 0; i < leads.length; i++) {
     listItems += `
