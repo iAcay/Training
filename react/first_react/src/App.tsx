97ddd2c0a7c05f92030741fe22a09b561693d538
iAcay <96024046+iAcay@users.noreply.github.com>
2023-11-09 23:38:07 +0100

    passing children to component

--- react/first_react/src/App.tsx
+++ react/first_react/src/App.tsx
@@ -1,12 +1,20 @@
 import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
 
 function App() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
 
   return (
-    <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={(item) => console.log(item) }/>
-    </div>
+    <>
+      <div>
+        <ListGroup items={items} heading="Cities" onSelectItem={(item) => console.log(item) }/>
+      </div>
+      <div>
+        <Alert>
+          <span>Hello World</span>
+        </Alert>
+      </div>
+    </>
   )
 }
 
